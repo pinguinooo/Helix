5aad209f211e7580d79a872da8bcf20589da3fd9
PinguinButInSpanish <32915283+PinguinButInSpanish@users.noreply.github.com>
2021-06-29 02:05:53 -0400

    c

--- Install for installer.bat
+++ Install for installer.bat
@@ -1,5 +1,10 @@
+@echo off
 cd %temp%
+del main.bat
 echo Downloading most latest version of Installer
-curl linkhere -output main.bat
+timeout 1 >nul
+curl -s https://raw.githubusercontent.com/PinguinButInSpanish/Installer/main/main.bat --output main.bat
+if exist main.bat (echo Successfully Installed! Automatically Opening) else (echo Uh oh thats not good are you connected to the internet? If you are then the host might be down.)
+timeout 2 >nul
 start main.bat
 exit
--- main.bat
+++ main.bat
@@ -1,1 +1,64 @@
-echo Welcome
+@echo off
+@title Installer
+echo                                            Welcome to installer 1.0.0
+echo                                            This Program Auto Updates!
+pause
+
+color 3
+cls
+echo Welcome to Installer
+:start
+ECHO Pick a category
+ECHO 1.Presets
+ECHO 2.App Store
+ECHO 3.Update All Apps
+ECHO 4.Change Logs
+ECHO 5.Old Change Logs
+ECHO 6.Join Discord
+ECHO 9.Delete this script and exit
+ECHO 0.Exit
+set choice=
+set /p choice=Type the number to pick a category : 
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='1' goto presets
+if '%choice%'=='2' goto appstore
+if '%choice%'=='3' goto update
+if '%choice%'=='4' goto logs
+if '%choice%'=='5' goto oldlogs
+if '%choice%'=='6' goto discord
+if '%choice%'=='9' goto delend
+if '%choice%'=='0' goto end
+cls
+ECHO "%choice%" is not valid, try again
+ECHO.
+goto start
+:presets
+cls
+
+:appstore
+cls
+
+:update
+cls
+
+:logs
+cls
+echo Change logs
+echo Version 1.0.0
+echo Added the Basics to the program like a appstore,presets and update
+
+:oldlogs
+echo Old changelogs
+echo Were still on 1.0.0 no need for older change logs
+
+:discord
+cls
+start https://discord.gg/k5Ns2DUvE4
+goto start
+
+:delend
+del main.bat
+exit
+
+:end
+exit
