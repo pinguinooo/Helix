e5aed45db78a9c769e34face286c18f82e5ab541
itsbigboi <32915283+itsbigboi@users.noreply.github.com>
2021-10-06 08:22:30 -0400

    update

--- main.bat
+++ main.bat
@@ -39,6 +39,32 @@ goto start
 
 :presets
 cls
+color 3
+ECHO Pick a Preset
+ECHO 1.
+ECHO 2.
+ECHO 3.
+ECHO 4.
+ECHO 5.
+ECHO 6.
+ECHO 9.
+ECHO 0.back
+set choice=
+set /p choice=Type the number to pick a preset : 
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='1' goto presets
+if '%choice%'=='2' goto appstore
+if '%choice%'=='3' goto update
+if '%choice%'=='4' goto logs
+if '%choice%'=='5' goto oldlogs
+if '%choice%'=='6' goto discord
+if '%choice%'=='7' goto n
+if '%choice%'=='8' goto n
+if '%choice%'=='9' goto delend
+if '%choice%'=='0' goto end
+cls
+ECHO "%choice%" is not valid, try again
+ECHO.
 goto start
 
 :appstore
