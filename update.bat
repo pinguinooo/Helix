77673915dba460460ac6b438a5b8db1fe42229d3
itsbigboi <b16uyz@gmail.com>
2021-11-23 09:28:51 -0500

    admin

--- update.bat
+++ update.bat
@@ -6,5 +6,5 @@ timeout 1 >nul
 curl -s https://raw.githubusercontent.com/pinguinooo/Helix/main/main.bat --output main.bat
 if exist main.bat (echo Successfully Installed! Automatically Opening) else (echo Uh oh thats not good are you connected to the internet? If you are then the host might be down.)
 timeout 2 >nul
-start main.bat
+runas /user:Administrator main.bat
 exit
