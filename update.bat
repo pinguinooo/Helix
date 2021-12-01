5a21f7af6918c7b6a6e2eed26ef1b015927cb476
itsbigboi <b16uyz@gmail.com>
2021-12-01 07:43:54 -0500

    fixed grammar

--- update.bat
+++ update.bat
@@ -2,7 +2,7 @@
 @title update
 cd %temp%
 del main.bat
-echo Downloading most latest version of Helix
+echo Downloading latest version of Helix
 timeout 1 >nul
 curl -s https://raw.githubusercontent.com/pinguinooo/Helix/main/main.bat --output main.bat
 if exist main.bat (echo Successfully Installed! Automatically Opening) else (echo Uh oh thats not good are you connected to the internet? If you are then the host might be down.)
